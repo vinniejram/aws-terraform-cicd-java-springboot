c1fb3605aa02dbdb4089991b97c8876fbe54cc4b
vinniejram <vinod.jothiram@gmail.com>
2022-10-19 14:31:59 -0600

    added ci\cd

---
+++ deploy.tf
@@ -0,0 +1,23 @@
+module "app" {
+    source = "./deployment"
+    application_name = "example-webapp"
+    application_version = "c341f5c"
+    artifact_bucket = "aston-lecture.springboot.simple-artifacts-west"
+    worker_ami = "ami-0abfe55c2cc86d5ad"
+    instance_profile = "allow_s3_ec2_github_demo"
+    backend_port = 8080
+    subnets = [
+    {
+      az     = "us-west-2a",
+      prefix = "10.1.1.0/24"
+    },
+    {
+      az     = "us-west-2b",
+      prefix = "10.1.2.0/24"
+    },
+    {
+      az     = "us-west-2c",
+      prefix = "10.1.3.0/24"
+    }
+  ]
+}
--- infra/aws/us-east-1/production/ecs.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecs_cluster" "prod" {
-  name = "${var.environment_name}-${var.ecs_cluster_name}"
-}
