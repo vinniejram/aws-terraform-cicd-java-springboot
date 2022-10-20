903aa1b676b7e3db66a69428cb6de95ce2955446
vinniejram <vinod.jothiram@gmail.com>
2022-10-20 13:07:39 -0600

    test tf deploy

--- deploy.tf
+++ deploy.tf
@@ -1,7 +1,7 @@
 module "cargarage" {
     source = "./deployment"
     application_name = "cargarage"
-    application_version = "58d4ec8"
+    application_version = "dcd899b"
     artifact_bucket = "aws-terraform-cicd-java-springboot-deploy-bucket"
     worker_ami = "ami-01c907a9b51818bd9"
     instance_profile = "allow_s3_ec2_github_demo"
