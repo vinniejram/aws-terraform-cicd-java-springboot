0eb5e12f8c0f6671dd816b801bc48519cb62b46a
vinniejram <vinod.jothiram@gmail.com>
2022-10-20 11:11:32 -0600

    test tf deploy

--- deployment/launch_configuration.tf
+++ deployment/launch_configuration.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "as_conf" {
   name_prefix   = "${var.application_name}-node-"
   image_id      = var.worker_ami
   instance_type = "t2.micro"
-  iam_instance_profile = var.instance_profile
+  #iam_instance_profile = var.instance_profile
   user_data = <<EOF
 #! /bin/bash
 aws s3 cp s3://${var.artifact_bucket}/${var.application_version} ./ --recursive
